a143d7bf18d1358e06dba1715950a48776b6c7ee
kris-what <kris-what@github.com>
2020-02-25 00:32:33 -0800

    optimized triNum by reducing a for loop

--- triNum.java
+++ triNum.java
@@ -7,17 +7,27 @@ class num {
         Arrays.sort(a);
         int n = a.length;
         for(int i = 0; i < n - 2; i++) {
-            for(int j = i + 1; j < n - 1; j++) {
-                for(int k = j + 1; k < n; k++) {
-                    if(a[i] + a[j] + a[k] == 0) {
-                        List<Integer> temp = new ArrayList<>();
-                        temp.add(Integer.valueOf(a[i]));
-                        temp.add(Integer.valueOf(a[j]));
-                        temp.add(Integer.valueOf(a[k]));
-                        if(!b.contains(temp))
-                            b.add(temp);
-                    }
-               }
+            int j = i + 1;
+            int k = n - 1;
+            /* two for loops by creating a while loop
+            instead of looping j & k individualy, put them together
+            and check until j == k
+             */
+            while(j < k) {
+                if(a[i] + a[j] + a[k] == 0) {
+                    List<Integer> temp = new ArrayList<>();
+                    temp.add(Integer.valueOf(a[i]));
+                    temp.add(Integer.valueOf(a[j]));
+                    temp.add(Integer.valueOf(a[k]));
+                    if(!b.contains(temp))
+                        b.add(temp);
+                    j++;
+                    k--;
+                }
+                else if(a[i] + a[j] + a[k] < 0)
+                    j++;
+                else
+                    k--;
             }
         }
         return b;
